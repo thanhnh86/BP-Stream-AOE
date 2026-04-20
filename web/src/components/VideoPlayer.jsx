4b1f7296cba67d2fb66a6415ce943baa629da28f
Nguyen Huy Thanh <nguyenhuythanh0806@gmail.com>
2026-04-20 09:52:34 +0700

    1

--- web/src/components/VideoPlayer.jsx
+++ web/src/components/VideoPlayer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import '@vidstack/react/player/styles/base.css';
 import '@vidstack/react/player/styles/default/theme.css';
 import '@vidstack/react/player/styles/default/layouts/video.css';
 import { MediaPlayer, MediaProvider } from '@vidstack/react';
